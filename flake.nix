1b563436d243f8b5dcb105ca0f899cc50972c825
eprader <56026248+eprader@users.noreply.github.com>
2024-03-08 12:15:21 +0100

    add nix flake for dependencies

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Apache Beam applications to be run with Apache Flink";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # cmake
+            # ninja
+          ];
+
+          shellHook = ''
+            export PS1="(nix-shell) $PS1" # NOTE: To communicate that a nix shell is active
+          '';
+        };
+      };
+    };
+}
