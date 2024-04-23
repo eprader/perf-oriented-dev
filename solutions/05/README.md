c253eb74af7cb513e0a8cc23e48b5465fbf41d73
eprader <56026248+eprader@users.noreply.github.com>
2024-04-24 01:15:08 +0200

    plotting

--- solutions/05/README.md
+++ solutions/05/README.md
@@ -15,7 +15,22 @@ ninja
 The `benchmarking-python/benchmarking_a.py` file will run all required programs **5** times and store the results in a `JSON` file.
 >[!NOTE]
 >The script will also create `.sh` and `.out` files run `rm *.out *.sh` to remove these files.
->The json files need to be moved into a data folder for the plotting script to pick up.
+>The json files need to be moved into a `data` folder for the plotting script to pick them up.
 >```bash
 >   mkdir data && mv *.json data
 >```
+
+To generate all plots run:
+```bash
+for program in "mmul" "nbody" "qap_chr15c" "delannoy_13" "npb_bt_w" "ssca2_15"
+do
+    python plotting_a.py $program
+done
+```
+
+![mmul](./mmul.png)
+![nbody](./nbody.png)
+![qap_chr15c](./qap_chr15c.png)
+![delannoy_13](./delannoy_13.png)
+![npb_bt_w](./npb_bt_w.png)
+![ssca2_15](./ssca2_15.png)
---
+++ solutions/05/plotting_a.py
@@ -0,0 +1,47 @@
+import json
+import sys
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(
+            "Usage: python plotting_a.py <file_name> (Example: python plotting_a.py qap_chr15c)"
+        )
+        sys.exit(1)
+
+optimisations = ["O0", "O1", "O2", "O3", "Ofast", "Os"]
+file_names = [f"{sys.argv[1]}_{optimisation}" for optimisation in optimisations]
+# Create bar plot
+# Create an empty plot
+plt.figure()
+
+# Iterate through the data and add bars one by one
+# for i in range(len(categories)):
+#     plt.bar(categories[i], values[i])
+
+# Add labels and title
+plt.xlabel("Optimisations")
+plt.ylabel("Average Runtime With StD")
+plt.title(sys.argv[1])
+for optimisation in optimisations:
+    file_name = f"{sys.argv[1]}_{optimisation}"
+    with open(f"./data/{file_name}.json", "r") as json_file:
+        data: List[Dict] = json.load(json_file)
+
+    runtimes: List[float] = [float(d["elapsed_wall_clock_time"]) for d in data]
+    mean_runtime: np.floating = np.mean(runtimes)
+
+    plt.bar(optimisation, mean_runtime)
+    plt.errorbar(
+        optimisation,
+        mean_runtime,
+        np.std(runtimes),
+        fmt=".",
+        color="Black",
+        capthick=4,
+        errorevery=1,
+    )
+plt.savefig(f"{sys.argv[1]}.png")
